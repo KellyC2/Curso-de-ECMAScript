8b92de5fd3ef44e23d2dbf5857ca979ab87733da
Kelly Condori <kellycondoricondori@gmail.com>
2023-11-05 11:35:05 -0500

    Actualizaciones de ECMAScrip7:2016---Exponentiation operator y array includes

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,7 @@
+function elevar (a,b=0){
+    console.log(a**b)
+}
+elevar(5,2);
+
+const data=3**4;
+console.log(data);
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,6 @@
+let myArray=[1,2,5,6,3,8,9];
+console.log(myArray.includes(0));
+console.log(myArray.includes(8));
+
+let nameArray=["María","Ana", "Martina", "Maricielo"]
+console.log(nameArray.includes("Martina"));
