2afe367d183c292a98d2782c21b551780b43a5ab
Kelly Condori <kellycondoricondori@gmail.com>
2023-11-05 14:10:22 -0500

    Uso de trailing commas para añadir item vacíos en un objeto

---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,18 @@
+//Los trailing commas consisten en comas al final de objetos o arraus que faciliten añadir nuevos elementos y evitar errores de sintaxis. 
+const usuario = {
+    name: 'Andres',
+    email: "andres@correo.com",
+    age: 23, //<-- Trailing comma
+}
+
+
+const nombres = [
+    "Andres",
+    "Valeria",
+    "Jhesly", //<-- Trailing comma
+]
+
+
+const array=[24,25,23,22, , , , , , ,];
+console.log(array);
+console.log(array.length);
