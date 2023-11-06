785a5232d6bc87909ee10a17ed84109415a29656
Kelly Condori <kellycondoricondori@gmail.com>
2023-11-06 16:59:25 -0500

    métod trim

---
+++ src/es10/01-trimstart-trimend.js
@@ -0,0 +1,5 @@
+//ELIMINAR ESPACIOS EN BLANCO DE UN STRING:
+//Existen métodos para eliminar espacios en blanco de un string. El método trim elimina espacios en blanco al inicio y al final, el método trimStart o trimLeft elimina los espacios al inicio. El método trimEnd o trimRight elimina los espacios al final.
+const saludo ="            Hello  WORD                "
+console.log(saludo.trimStart());
+console.log(saludo.trimEnd());
